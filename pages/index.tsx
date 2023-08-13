8b6cf2dd0d251d6eb31afea611256db2482ce05f
evilhaem <evilhaem@gmail.com>
2023-08-13 17:08:38 +0800

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,94 @@
+import { useEffect, useRef, useState } from 'react';
+import { Group, Stack, Text, Image, Progress, Button, Code, Skeleton,} from '@mantine/core';
+import { useClipboard } from '@mantine/hooks';
+import { Dropzone, IMAGE_MIME_TYPE } from '@mantine/dropzone';
+import { createWorker } from 'tesseract.js';
+
+const Home = () => {
+    const [loading, setLoading] = useState(false);
+  const [imageData, setImageData] = useState<null | string>(null);
+  const clipboard = useClipboard({ timeout: 2500 });
+  const loadFile = (file: File) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const imageDataUri = reader.result;
+      setImageData(imageDataUri as string);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const [progress, setProgress] = useState(0);
+  const [progressLabel, setProgressLabel] = useState('idle');
+  const [ocrResult, setOcrResult] = useState('');
+
+  const workerRef = useRef<Tesseract.Worker | null>(null);
+  useEffect(() => {
+    workerRef.current = createWorker({
+      logger: message => {
+        if ('progress' in message) {
+          setProgress(message.progress);
+          setProgressLabel(message.progress == 1 ? 'Done' : message.status);
+        }
+      }
+    });
+    return () => {
+      workerRef.current?.terminate();
+      workerRef.current = null;
+    }
+  }, []);
+
+  const handleExtract = async () => {
+    setLoading((l) => !l);
+    setProgress(0);
+    setProgressLabel('starting');
+
+    const worker = workerRef.current!;
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    
+    const response = await worker.recognize(imageData!);
+    setOcrResult(response.data.text);
+    console.log(response.data);
+    setLoading((l) => !l)
+  };
+
+  return (<>
+    <Group align='initial' style={{ padding: '30px' }}>
+      <Stack style={{ flex: '1' }}>
+        <Dropzone
+          onDrop={(files) => loadFile(files[0])}
+          accept={IMAGE_MIME_TYPE}
+          multiple={false}
+        >{() => (
+          <Text size="xl" inline>
+            Drag image here or click to select file
+          </Text>
+        )}</Dropzone>
+        <Button disabled={!imageData || !workerRef.current} onClick={handleExtract}>Extract</Button>
+        <Progress value={progress * 100}/>
+        <Code style={{ textAlign: 'center', fontFamily: 'inherit'}}>{progressLabel.toLowerCase()}</Code>
+        
+        
+        <Text size='xl'>RESULT: </Text>
+        <Skeleton visible={loading}>
+        <Code style={{minHeight: '100px', fontSize: '16px'}} block>{ocrResult}</Code>
+        </Skeleton>
+        <Button color={clipboard.copied ? 'teal' : 'blue'} onClick={() => clipboard.copy(ocrResult)}>
+            {clipboard.copied ? 'Copied' : 'Copy'}
+        </Button>
+
+        </Stack>
+        
+        <Stack style={{ flex: '1' }}>
+          <Text size='xl'>IMAGE PREVIEW: </Text>
+          {!!imageData && <Image src={imageData}/>}
+          {!imageData && <Image height={'340px'} alt="empty" withPlaceholder />}
+        </Stack>
+        
+    </Group>
+  </>);
+}
+
+export default Home;
+
